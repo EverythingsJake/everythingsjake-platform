26de9b57a88135b90d48a139bbb5e8fbaae23b2d
Jake Piazza <jacob.a.piazza@gmail.com>
2026-01-08 17:27:19 -0500

    Refactor layout and styling: integrate local font and update page structure

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +12,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const whitePalmer = localFont({
+  src: "./fonts/White-Palmer.otf",
+  variable: "--font-white-palmer",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,8 +31,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={whitePalmer.variable}>
+      <body>
         {children}
       </body>
     </html>
